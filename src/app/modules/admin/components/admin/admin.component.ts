d7629070b7ab510b33458f17977acc6e4ec718eb
Aslin <aslinkarmacharya@gmail.com>
2025-04-01 12:59:10 +0545

    admin fix

--- src/app/modules/admin/components/admin/admin.component.ts
+++ src/app/modules/admin/components/admin/admin.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
-
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit(): void {
+    if (!this.authService.isLoggedIn()) {
+      this.router.navigate(['/login']);
+    }
+  }
 }
