fd61f6d7cf9204a085193d76b9a67f2e7a561387
Aslin <aslinkarmacharya@gmail.com>
2025-04-01 14:35:42 +0545

    Session time added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,17 +12,17 @@ interface Admin {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private apiUrl = environment.apiUrl;
+  private sessionTime = 30 * 60 * 1000;
 
-  constructor(
-    private router: Router, 
-    private http: HttpClient) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   private setToken(token: string) {
     localStorage.setItem('token', token);
+    this.sessionTimeout();
   }
 
   getToken(): string | null {
@@ -47,32 +47,48 @@ export class AuthService {
     this.router.navigate(['login']);
   }
 
-  signup({ email, password }: { email: string; password: string }): Observable<any> {
+  signup({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }): Observable<any> {
     const newAdmin = { email, password };
-    return this.http.post<Admin>(`${this.apiUrl}/admins`, newAdmin)
-      .pipe(
-        map((admin) => {
+    return this.http.post<Admin>(`${this.apiUrl}/admins`, newAdmin).pipe(
+      map((admin) => {
+        this.setToken(admin.id);
+        this.setAdminId(admin.id);
+        return { success: true, admin };
+      }),
+      catchError((error) => throwError(() => new Error('Signup failed')))
+    );
+  }
+
+  login({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }): Observable<any> {
+    return this.http.get<Admin[]>(`${this.apiUrl}/admins?email=${email}`).pipe(
+      map((admins) => {
+        const admin = admins[0];
+        if (admin && admin.password === password) {
           this.setToken(admin.id);
           this.setAdminId(admin.id);
           return { success: true, admin };
-        }),
-        catchError(error => throwError(() => new Error('Signup failed')))
-      );
+        }
+        throw new Error('Invalid credentials');
+      }),
+      catchError((error) => throwError(() => new Error('Invalid credentials')))
+    );
   }
 
-  login({ email, password }: { email: string; password: string }): Observable<any> {
-    return this.http.get<Admin[]>(`${this.apiUrl}/admins?email=${email}`)
-      .pipe(
-        map((admins) => {
-          const admin = admins[0];
-          if (admin && admin.password === password) {
-            this.setToken(admin.id);
-            this.setAdminId(admin.id);
-            return { success: true, admin };
-          }
-          throw new Error('Invalid credentials');
-        }),
-        catchError(error => throwError(() => new Error('Invalid credentials')))
-      );
+  sessionTimeout() {
+    setTimeout(() => {
+      this.logout();
+    }, this.sessionTime);
   }
 }
